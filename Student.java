2d559de0c15bd9faa12821758a665f1a3c4747a1
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-04-16 17:25:10 -0400

    Added Student

--- Student.java
+++ Student.java
@@ -1,4 +1,15 @@
 public class Student
 {
   public String firstName;
+  public int gradeLevel;
+  
+  public Student(String name, String grade)
+  {
+     firstName =name;
+     gradeLevel = grade;
+  }
+
+  public String toString()
+  {
+    return firstName + "" + gradeLevel;
 }
