20c7ac07ae9db717073a69f9cb11d05a6ebae606
LinYinXue <166444717+LinYinXue@users.noreply.github.com>
2024-04-17 16:55:54 -0400

    added rectangle

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle
+{
+  private int width;
+  private int height;
+  
+  public Rectangle(int recWidth, int recHeight)
+  {
+     width = recWidth;
+     height = recHeight;
+  }
+
+  public int getArea()
+  {
+     return width* height;
+
+  public String toString()
+  {
+    return "width: " + width + "height: " + height;
+}
